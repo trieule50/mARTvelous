0e42c525dd067d16d235e1c00092ea9207c4fb57
trieule50 <trieule50@gmail.com>
2021-11-17 12:32:32 -0500

    Add background color on landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Navigation from './components/Navigation';
-
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <main>
-        
+        <Home/>
       </main>
     </div>
   );
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+const Home = () =>{
+    return(
+        <div className='landing-container'>
+            <div className='landing-item'>
+                <h1>mARTvelous</h1>
+            </div>
+            <div className='landing-item'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
