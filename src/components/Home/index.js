709fbe4f06d3e5a50f18bd5c4ba27ce6dc14cd66
trieule50 <trieule50@gmail.com>
2021-11-17 12:44:01 -0500

    Update background color

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
+import logo from '../../images/mARTvelous_logo.png'
+
 const Home = () =>{
     return(
         <div className='landing-container'>
             <div className='landing-item'>
-                <h1>mARTvelous</h1>
+                <h1>Start Creating</h1>
             </div>
             <div className='landing-item'>
-
+            <img  className='landing-pic' src={logo} alt="mARTvelous"/>
             </div>
         </div>
     )
