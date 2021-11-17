63c4d156c1cd634a854e0c4f27e86857a6268b14
trieule50 <trieule50@gmail.com>
2021-11-17 12:22:35 -0500

    Add styling to Nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,16 +1,17 @@
+import logo from '../../images/mARTvelous_logo.png'
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 const Navigation = () => {
     return(
-        <Navbar bg="light" expand="lg">
+        <Navbar bg='light' expand="lg">
             <Container>
-                <Navbar.Brand href="/home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="/home"><img  className='logo' src={logo} alt="mARTvelous"/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">PaintBox</Nav.Link>
-                        <Nav.Link href="#link">FAQ</Nav.Link>
+                    <Nav className="ms-auto">
+                        <Nav.Link class="nav-link" href="/home">Home</Nav.Link>
+                        <Nav.Link class="nav-link" href="/paintbox">PaintBox</Nav.Link>
+                        <Nav.Link class="nav-link" href="/faq">FAQ</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
