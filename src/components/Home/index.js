d5efa92d57630a54d4654c42357cf9302a5b5448
trieule50 <trieule50@gmail.com>
2021-11-18 11:18:56 -0500

    Delete images

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,16 @@
-import logo from '../../images/mARTvelous_logo.png'
+import logo from '../../images/mARTvelous_logo.png';
 
 const Home = () =>{
     return(
         <div className='landing-container'>
             <div className='landing-item'>
                 <h1>Start Creating</h1>
+                <h1>Let Out your Inner Artist</h1>
             </div>
-            <div className='landing-item'>
-            <img  className='landing-pic' src={logo} alt="mARTvelous"/>
-            </div>
+           <div className='landing-item'>
+           </div>
         </div>
+        
     )
 }
 
