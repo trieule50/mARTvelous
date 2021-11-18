c98969b4bade20081efee4c9bc57365dda296b76
trieule50 <trieule50@gmail.com>
2021-11-18 12:42:42 -0500

    Add a call to action

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,10 +4,11 @@ import UncontrolledLotties from '../UncontrolledLottie';
 const Home = () =>{
     return(
         <div className='landing-container'>
-            <div className='landing-item'>
-                <h1>Start Creating</h1>
-                <h1>Let Out your Inner Artist</h1>
-                <a href='/paintbox'>PaintBox</a>
+            <div className='landing-item one'>
+                <h1>Art is for Everyone</h1>
+                <p className='call-to-action statement-one'>Whether you need to take a break, do a quick sketch, or express yourself, mARTvelous will help you and provide the tools necessary.</p>
+                <p className='call-to-action statement-two'>Let out your inner artist and start drawing by navigating to the PaintBox.</p>
+                <a href='/paintbox' className='button'>PaintBox</a>
             </div>
            <div className='landing-item'>
                <UncontrolledLotties/>
