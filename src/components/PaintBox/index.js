8759eefc7241e333da3f46517827913e5cf9cf76
trieule50 <trieule50@gmail.com>
2021-11-18 16:53:15 -0500

    Style paintbox

--- src/components/PaintBox/index.js
+++ src/components/PaintBox/index.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
+import { Container } from "react-bootstrap";
+import CanvasDraw from "react-canvas-draw";
+
+
+
 const PaintBox = () => {
+
+    const [setting, setSetting] = useState({
+        width: 500,
+        height: 500,
+        color: "rgba(0,0,0,1)",
+    });
+
     return(
         <div>
             <h1>PaintBox</h1>
+            <Container className='canvas'>
+                <CanvasDraw
+                canvasWidth={setting.width}
+                canvasHeight={setting.height}
+                gridColor={setting.color}
+                />
+            </Container>
         </div>
     )
 }
