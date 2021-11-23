4394b1ced37f0dab68ecee14dd5eb34784842340
trieule50 <trieule50@gmail.com>
2021-11-22 19:05:30 -0500

    Edit padding

--- src/components/PaintBox/index.js
+++ src/components/PaintBox/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import CanvasDraw from "react-canvas-draw";
 
 
@@ -9,19 +9,34 @@ const PaintBox = () => {
     const [setting, setSetting] = useState({
         width: 500,
         height: 500,
-        color: "rgba(0,0,0,1)",
+        brushColor: "#444",
+        gridColor: "rgba(0,0,0,1)",
+        gridSizeX: 2,
+        gridSizeY: 2
     });
 
     return(
         <div className='paintbox-main'>
             <h1>PaintBox</h1>
-            <Container className='canvas'>
+            <div className='canvas'>
+            <div className='canvas-item'>
                 <CanvasDraw
                 canvasWidth={setting.width}
                 canvasHeight={setting.height}
-                gridColor={setting.color}
+                gridColor={setting.gridColor}
+                gridSizeX={setting.gridSizeX}
+                gridSizeY={setting.gridSizeY}
                 />
-            </Container>
+            </div>
+            <div className='canvas-item'>
+                <Container>
+                    <Row className='canvas-item-erase'><Col>Erase</Col></Row>
+                    <Row className='canvas-item-line-weight'><Col>Line Weight</Col></Row>
+                    <Row className='canvas-item-line-color'><Col>Color</Col></Row>
+                    <Row className='canvas-item-image'><Col>Image</Col></Row>
+                </Container>
+            </div>
+            </div>
         </div>
     )
 }
