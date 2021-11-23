e956f43330a8858ada63187739275ea7a50c9689
trieule50 <trieule50@gmail.com>
2021-11-22 20:15:52 -0500

    Edit Line color

--- src/components/PaintBox/index.js
+++ src/components/PaintBox/index.js
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react';
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
 import CanvasDraw from "react-canvas-draw";
 
 
 
 const PaintBox = () => {
 
+    const handleColorChange = (event) =>{
+        // event.preventDefault();
+        setUserChoice(event.target.value)
+    }
+
+    const [userChoice, setUserChoice] = useState('');
+
     const [setting, setSetting] = useState({
         width: 500,
         height: 500,
-        brushColor: "#444",
         gridColor: "rgba(0,0,0,1)",
         hideGrid: true,
     });
@@ -24,14 +30,27 @@ const PaintBox = () => {
                 canvasHeight={setting.height}
                 gridColor={setting.gridColor}
                 hideGrid={setting.hideGrid}
+                brushColor={userChoice}
                 />
             </div>
             <div className='canvas-item'>
                 <Container>
-                    <Row className='canvas-item-erase'><Col>Erase</Col></Row>
-                    <Row className='canvas-item-line-weight'><Col>Line Weight</Col></Row>
-                    <Row className='canvas-item-line-color'><Col>Color</Col></Row>
-                    <Row className='canvas-item-image'><Col>Image</Col></Row>
+                    <Row className='canvas-item-erase'>
+                        <Col>Erase</Col>
+                    </Row>
+                    <Row className='canvas-item-line-weight'>
+                        <Col>Line Weight</Col>
+                    </Row>
+                    <Row className='canvas-item-line-color'>
+                        <Col>
+                            <label for='line-color'>Line Color</label>
+                            <input type="color" id="body" name="body"
+                            onChange={handleColorChange}/>
+                        </Col>
+                    </Row>
+                    <Row className='canvas-item-image'>
+                        <Col>Image</Col>
+                    </Row>
                 </Container>
             </div>
             </div>
